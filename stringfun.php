c84bdbbbd8635608ec35e1da0afe3fa8f7ef76e1
srnit <ramansudhanshu150@gmail.com>
2017-01-18 21:50:09 +0530

    this fuction calculate the % of similarites between two string

---
+++ stringfun.php
@@ -0,0 +1,7 @@
+/*this function calculate the % of of simalirities between two string.*/
+<?php
+$string_one="sudhanshu rmam";
+$string_two="sudhanshu raman";
+similar_text($string_one,$string_two,$result);
+echo $result;
+?>
